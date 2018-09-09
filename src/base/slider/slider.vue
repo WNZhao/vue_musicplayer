14e437054e101437f08e9a2638e81d8dbd29e826
willzhao <nanan_zhao@163.com>
2018-09-09 20:56:16 +0800

    slider

--- src/base/slider/slider.vue
+++ src/base/slider/slider.vue
@@ -1,15 +1,30 @@
 <template>
-  <div class="">
+  <div class="slider-box"
+       ref="sliderBox">
     <slot></slot>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-  export default {
-    name: "slider"
+export default {
+  name: "slider",
+  mounted () {
+    console.log(this.$refs.sliderBox.offsetWidth)
   }
+}
 </script>
 
-<style scoped rel="stylesheet/stylus" lang="stylus">
-
+<style scoped rel="stylesheet/stylus" lang='stylus'>
+.slider-box
+  height 400px
+  overflow hidden
+  position relative
+  .slider-item
+    height 100%
+    .slider-img-link
+      display block
+      height 100%
+      img
+        width 100%
+        height 100%
 </style>
--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,12 +1,16 @@
 <template>
-  <div class="recommend" ref="recommend">
+  <div class="recommend"
+       ref="recommend">
     <div>
-      <div v-if="recommends.length" class="slider-wrapper"
+      <div v-if="recommends.length"
+           class="slider-wrapper"
            ref="sliderWrapper">
         <slider>
-          <div v-for="item in recommends"
+          <div class="slider-item"
+               v-for="item in recommends"
                :key="item.id">
-            <a :href="item.linkUrl">
+            <a class="slider-img-link"
+               :href="item.linkUrl">
               <img class="needsclick"
                    @load="loadImage"
                    :src="item.picUrl">
@@ -25,121 +29,101 @@
 </template>
 
 <script type="text/ecmascript-6">
-  import Slider from 'base/slider/slider'
-  import {getRecommend} from 'api/recommend'
-  import {ERR_OK} from 'api/config'
+import Slider from 'base/slider/slider'
+import { getRecommend } from 'api/recommend'
+import { ERR_OK } from 'api/config'
 
-  export default {
-    data() {
-      return {
-        recommends: [],
-        discList: []
-      }
-    },
-    created() {
-      this._getRecommend()
-    },
-    methods: {
-      handlePlaylist(playlist) {
-        const bottom = playlist.length > 0 ? '60px' : ''
-
-        this.$refs.recommend.style.bottom = bottom
-      },
-      loadImage() {
-        if (!this.checkloaded) {
-          this.checkloaded = true
-        }
-      },
-      selectItem(item) {
-        this.$router.push({
-          path: `/recommend/${item.dissid}`
-        })
-        this.setDisc(item)
-      },
-      _getRecommend() {
-        getRecommend().then((res) => {
-          if (res.code === ERR_OK) {
-            this.recommends = res.data.slider
-          }
-        })
-      },
-      _getDiscList() {
-
-      }
-    },
-    components: {
-      Slider
+export default {
+  data () {
+    return {
+      recommends: [],
+      discList: []
     }
+  },
+  created () {
+    this._getRecommend()
+  },
+  methods: {
+    handlePlaylist (playlist) {
+      const bottom = playlist.length > 0 ? '60px' : ''
+
+      this.$refs.recommend.style.bottom = bottom
+    },
+    loadImage () {
+      if (!this.checkloaded) {
+        this.checkloaded = true
+      }
+    },
+    selectItem (item) {
+      this.$router.push({
+        path: `/recommend/${item.dissid}`
+      })
+      this.setDisc(item)
+    },
+    _getRecommend () {
+      getRecommend().then((res) => {
+        if (res.code === ERR_OK) {
+          this.recommends = res.data.slider
+        }
+      })
+    },
+    _getDiscList () {
+
+    }
+  },
+  components: {
+    Slider
   }
+}
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
-  @import '~common/stylus/variable';
+@import '~common/stylus/variable'
 
-  .recommend {
-    position: fixed;
-    width: 100%;
-    top: 88px;
-    bottom: 0;
-
-    .recommend-content {
-      height: 100%;
-      overflow: hidden;
-
-      .slider-wrapper {
-        position: relative;
-        width: 100%;
-        overflow: hidden;
-      }
-
-      .recommend-list {
-        .list-title {
-          height: 65px;
-          line-height: 65px;
-          text-align: center;
-          font-size: $font-size-medium;
-          color: $color-theme;
-        }
-
-        .item {
-          display: flex;
-          box-sizing: border-box;
-          align-items: center;
-          padding: 0 20px 20px 20px;
-
-          .icon {
-            flex: 0 0 60px;
-            width: 60px;
-            padding-right: 20px;
-          }
-
-          .text {
-            display: flex;
-            flex-direction: column;
-            justify-content: center;
-            flex: 1;
-            line-height: 20px;
-            overflow: hidden;
-            font-size: $font-size-medium;
-
-            .name {
-              margin-bottom: 10px;
-              color: $color-text;
-            }
-
-            .desc {
-              color: $color-text-d;
-            }
-          }
-        }
-      }
-
-      .loading-container {
-        position: absolute;
-        width: 100%;
-        top: 50%;
-        transform: translateY(-50%);
-      }
-    }
-  }
+.recommend
+  position fixed
+  width 100%
+  top 88px
+  bottom 0
+  .recommend-content
+    height 100%
+    overflow hidden
+    .slider-wrapper
+      position relative
+      width 100%
+      overflow hidden
+    .recommend-list
+      .list-title
+        height 65px
+        line-height 65px
+        text-align center
+        font-size $font-size-medium
+        color $color-theme
+      .item
+        display flex
+        box-sizing border-box
+        align-items center
+        padding 0 20px 20px 20px
+        .icon
+          flex 0 0 60px
+          width 60px
+          padding-right 20px
+        .text
+          display flex
+          flex-direction column
+          justify-content center
+          flex 1
+          line-height 20px
+          overflow hidden
+          font-size $font-size-medium
+          .name
+            margin-bottom 10px
+            color $color-text
+          .desc
+            color $color-text-d
+    .loading-container
+      position absolute
+      width 100%
+      top 50%
+      transform translateY(-50%)
 </style>
