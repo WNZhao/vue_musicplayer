09c4507c51835ce6a5cb2d5baf0c1f74deeac0ef
willzhao <nanan_zhao@163.com>
2018-09-17 23:23:53 +0800

    slider to right

--- src/base/slider/slider.vue
+++ src/base/slider/slider.vue
@@ -2,21 +2,27 @@
   <div class="slider-box"
        :style="boxWidths"
        ref="sliderBox">
-    <slot :width="itemWidth"></slot>
+    <slot :width="itemWidth+'px'"></slot>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+
 export default {
+  props: ["len", "recommends"],
   data () {
     return {
+      currentIdx: 0,
       boxWidths: {
-        width: 'auto'
+        width: 'auto',
+        left: 0
       },
-      itemWidth: 'auto'
+      itemWidth: 'auto',
+      direction: -1, // 1向右 -1向左
+      step: 25, //步长
+      isAuto: true //轮播自动播放
     }
   },
-  props: ["len", "recommends"],
   name: "slider",
   methods: {
     computeWidth () {
@@ -24,8 +30,8 @@ export default {
       // document.defaultView.getComputedStyle(html,null).width
       const wrapperWidth = window.innerWidth;
       const boxWidths = len * wrapperWidth + wrapperWidth;
-      this.boxWidths.width = boxWidths + 'px';
-      this.itemWidth = wrapperWidth + 'px';
+      this.boxWidths.width = boxWidths;
+      this.itemWidth = wrapperWidth;
       return boxWidths;
     },
     throttle (fn, sec = 300) {
@@ -36,14 +42,83 @@ export default {
           fn()
         }, sec);
       }
+    },
+    calculPos () {
+      const { step } = this;
+      const itemWidth = this.itemWidth;
+      // 向左
+      /* if (this.currentIdx + 1 >= this.len) {
+        this.currentIdx = 0;
+      }
+      let currentLeft = this.currentIdx * itemWidth;
+      this.currentIdx = this.currentIdx + 1;
+      let targetLeft = this.currentIdx * itemWidth;
+      const interval = setInterval(() => {
+        if (targetLeft > currentLeft) {
+          currentLeft += step;
+        } else {
+          currentLeft = targetLeft;
+        }
+        this.boxWidths.left = this.direction * currentLeft + 'px';
+        if (currentLeft == targetLeft) {
+          // 移动到指定位置时停止移动
+          clearInterval(interval);
+          if (this.isAuto === true) {
+            let t = setTimeout(() => {
+              this.calculPos();
+              if (t) {
+                clearTimeout(t);
+              }
+            }, 1500);
+          }
+        }
+      }, 25); */
+      // 向右
+      if (this.currentIdx == 0) {
+        this.currentIdx = this.len;
+      }
+
+      let currentLeft = this.currentIdx * itemWidth;
+      this.currentIdx = this.currentIdx - 1;
+      let targetLeft = this.currentIdx * itemWidth;
+      const interval = setInterval(() => {
+        if (targetLeft < currentLeft) {
+          currentLeft -= step;
+        } else {
+          currentLeft = targetLeft;
+        }
+        this.boxWidths.left = this.direction * currentLeft + 'px';
+        if (currentLeft == targetLeft) {
+          // 移动到指定位置时停止移动
+          clearInterval(interval);
+          if (this.isAuto === true) {
+            let t = setTimeout(() => {
+              this.calculPos();
+              if (t) {
+                clearTimeout(t);
+              }
+            }, 1500);
+          }
+        }
+      }, 25);
+    },
+    adjustWidth () {
+      const that = this;
+      that.computeWidth();
+      window.onresize = this.throttle(function () {
+        that.computeWidth()
+      }, 300)
+    },
+    parepareSlider () {
+      this.adjustWidth();
+      const { isAuto } = this;
+      if (isAuto) {
+        const t = setTimeout(() => { this.calculPos(); clearTimeout(t) }, 1000)
+      }
     }
   },
   mounted () {
-    const that = this;
-    that.computeWidth();
-    window.onresize = this.throttle(function () {
-      that.computeWidth()
-    }, 300)
+    this.parepareSlider();
   },
   destroyed () {
     window.onresize = null
@@ -52,16 +127,22 @@ export default {
 </script>
 
 <style scoped rel="stylesheet/stylus" lang='stylus'>
-.slider-box
-  height 260px
-  overflow hidden
+.slider-wrapper
   position relative
-  .slider-item
-    height 100%
-    .slider-img-link
-      display block
+  .slider-box
+    height 260px
+    overflow hidden
+    position absolute
+    white-space nowrap
+    font-size 0
+    top 0
+    .slider-item
       height 100%
-      img
-        width 100%
+      display inline-block
+      .slider-img-link
+        display block
         height 100%
+        img
+          width 100%
+          height 100%
 </style>
