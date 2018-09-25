019253e7f6d7c6bf87c9a850d82642a5eaee9e91
willzhao <nanan_zhao@163.com>
2018-09-25 23:35:43 +0800

    添加歌单列表数据（修改vue2.5以上dev-server.js的方法是在webpack.dev.conf.js中添加相关内容）

--- src/api/recommend.js
+++ src/api/recommend.js
@@ -1,5 +1,6 @@
 /* eslint-disable */
-import jsonp from 'common/js/jsonp'
+import jsonp from 'common/js/jsonp';
+import axios from 'axios';
 import {
   commonParams,
   options
@@ -14,3 +15,31 @@ export function getRecommend() {
   })
   return jsonp(url, data, options)
 }
+
+export function getDiscList() {
+    // const url = 'https://c.y.qq.com/splcloud/fcgi-bin/fcg_get_diss_by_tag.fcg'
+    // const url = 'https://c.y.qq.com/splcloud/fcgi-bin/fcg_get_diss_tag_conf.fcg'
+    const url = '/api/getDiscList'
+    const data  = Object.assign({}, commonParams, {
+        platform: 'yyq',
+        hostUin: 0,
+        sin: 0,
+        ein: 29,
+        sortId: 5,
+        needNewCode: 0,
+        categoryId: 10000000,
+        rnd: Math.random(),
+        format: 'json'
+      })
+    // return jsonp(url, data, options)
+    return axios.get(url,{
+        params: data
+    }).then((res) => {
+        return Promise.resolve(res.data);
+    })
+}
+
+function getPlaylistTags(data){
+    console.log(data)
+}
+window.getPlaylistTags= getPlaylistTags;
--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -35,7 +35,7 @@
 
 <script type="text/ecmascript-6">
 import Slider from 'base/slider/slider'
-import { getRecommend } from 'api/recommend'
+import { getRecommend, getDiscList } from 'api/recommend'
 import { ERR_OK } from 'api/config'
 
 export default {
@@ -47,6 +47,7 @@ export default {
   },
   created () {
     this._getRecommend()
+    this._getDiscList()
   },
   methods: {
     handlePlaylist (playlist) {
@@ -73,7 +74,12 @@ export default {
       })
     },
     _getDiscList () {
-
+      getDiscList().then((res) => {
+          debugger;
+          if (res.code === ERR_OK){
+              console.log(res.data.list);
+          }
+      })
     }
   },
   components: {
